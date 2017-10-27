5aee50e75981f588dad5d3de44419bf2400db9f3
PedrosWits <ppintodasilva@gmail.com>
2017-10-27 16:54:42 +0000

    Circumvented issues with restore/attach of database by implementing separate functions. As container.exec_run does not return the result of the operation we can't verify that the query was run successfully and stop the server in case it didn't. Maybe in a feature release we can integrate the several methods a bigger one. Added file exist/not-exist checks to avoid bad operations. Fixed corresponding documentation.

--- server.py
+++ server.py
@@ -6,6 +6,8 @@ import os
 import stat
 import click
 import docker
+import ast
+import sys
 
 ###############################################
 #
@@ -24,6 +26,51 @@ bakfile_path = os.getcwd() + "/bakfile"
 dbfiles_container_path = '/mnt/anpr-mssql'
 bakfile_container_path = '/mnt/anpr-bak'
 
+default_db_namemap = {'CortexDBWarehouse' : 'CortexDBWarehouse_Primary.mdf',
+                      'BLOB' : 'CortexDBWarehouse_BLOB.mdf',
+                      'DATA' : 'CortexDBWarehouse_DATA.mdf',
+                      'INDEX' : 'CortexDBWarehouse_INDEX.mdf',
+                      'CortexDBWarehouse_log' : 'CortexDBWarehouse_Log.ldf'}
+
+###############################################
+#
+#
+#   Queries
+#
+#
+###############################################
+
+def query_restoredb_builder(dbname, db_namemap, bakfile_name): 
+    move_lines = ""
+    for dbComponent, componentFilename in db_namemap.iteritems():
+        move_lines += "MOVE ''{}'' TO ''{}'', ".format(
+                dbComponent, 
+                "{}/{}".format(dbfiles_container_path,componentFilename))
+    move_lines += "STATS = 10"
+    sql_function = " ".join(["RESTORE DATABASE {}".format(dbname),
+                            "FROM DISK = ''{}''".format("{}/{}".format(bakfile_container_path, bakfile_name)),
+                            "WITH {}".format(move_lines)])
+    return "\n".join(["USE master",
+                      "GO",
+                      "EXEC('{}')".format(sql_function),
+                      "GO"])
+
+def query_attachdb_builder(dbname, db_namemap):
+    filenames = list(db_namemap.values())
+    filename_lines = ""
+    for i in range(0, len(filenames), 1):
+        if i == len(filenames)-1:
+            separator = ';'
+        else:
+            separator = ','
+        filename_lines += "@filename{} = \"{}\"{} ".format(i+1, "".join([dbfiles_container_path, "/", filenames[i]]), separator)
+
+    return " ".join(["EXEC sp_attach_db @dbname ='{}',".format(dbname),
+                     filename_lines])
+
+def query_restore_progress_builder():
+    return "SET NOCOUNT ON;SELECT start_time,cast(percent_complete as int) as progress,dateadd(second,estimated_completion_time/1000, getdate()) as estimated_completion_time, cast(estimated_completion_time/1000/60 as int) as minutes_left FROM sys.dm_exec_requests r WHERE r.command='RESTORE DATABASE'"
+
 ###############################################
 #
 #
@@ -37,10 +84,11 @@ def getContainer():
     try:
         return client.containers.get(container_name)
     except docker.errors.NotFound as e:
-        click.echo(e)
+        # click.echo(e)
+        return None
     except docker.errors.APIError as e2:
-        click.echo(e2)
-    return None
+    #    click.echo(e2)
+        return None
 
 ###############################################
 #
@@ -139,63 +187,146 @@ def pull():
 @anpr.command('start', short_help="Start the anpr-mssql-server")
 @click.option('--password', '-p',
              type = str,
-             envvar = 'SQL_SERVER_PASSWORD',
-             required = True)
-@click.argument('mode',
-                required = True,
-                type = click.Choice(['restore', 'attach']),
-                default = 'attach')
-def run_container(mode, password):
+             envvar = 'SQL_PASSWORD',
+             required = True,
+             help = "Database password for SA user. Read from environment variable 'SQL_PASSWORD'")
+def run_container(password):
     """
     Run a new container named "anpr-mssql-server".
 
-    The microsoft/mssql-server-linux docker image is an official image for Microsoft SQL Server on Linux for Docker Engine and is designed to be used in real production environments and support real workloads. Therefore, I think we can assume that the container can be run for long periods of time and won't be restarted frequently. As a result, we do not persist the master database, which records all the system-level information for a SQL Server system. Instead, the openstack volumes containing the dbfiles should be used to store the database fles. Then, on starting a new container we either re-create the anpr database by attaching the existing database files that should be mounted in 'dbfiles' (mdf, ldf), or by restoring the backup file that should be mounted in 'bakfile'. This behavior is specified by the parameter 'mode' and should be set to 'restore' or 'attach' depending on the format of the anpr data resources available: if bakfile then restore, else if mdf,ldf files then attach.
-
+    The microsoft/mssql-server-linux docker image is an official image for Microsoft SQL Server on Linux for Docker Engine and is designed to be used in real production environments and support real workloads. Therefore, I think we can assume that the container can be run for long periods of time and won't be restarted frequently. As a result, we do not persist the master database, which records all the system-level information for a SQL Server system. Instead, the openstack volumes containing the dbfiles should be used to store the database fles.
+    
     The server is configured similarly to what is documented at https://hub.docker.com/r/microsoft/mssql-server-linux/.
 
     The system administrator password must be provided via the environment variable 'SQL_SERVER_PASSWORD'.
+
+    After initiating the container use commands 'restore' and 'attach' to restore and attach the anpr database respectively.
     """
+    # Test if container exists
     container = getContainer()
     if container:
         click.echo("Server is already running")
         return
-    if mode == "attach":
-        # If not mounted - exit
-        volumes = {dbfiles_path : {'bind' : dbfiles_container_path,
-                                   'mode' : 'ro'}}
-    else:
-        # If not mounted - exit
-        volumes = {bakfile_path : {'bind' : bakfile_container_path,
-                                    'mode' : 'ro'},
-                   dbfiles_path : {'bind' : dbfiles_container_path,
-                                   'mode' : 'rw'}}
+    # Verify build mounts
+    # Build map of mountpoints for docker.run
+    volumes = {bakfile_path : {'bind' : bakfile_container_path,
+                               'mode' : 'ro'},
+               dbfiles_path : {'bind' : dbfiles_container_path,
+                               'mode' : 'rw'}}
+    # Get the docker client
     client = docker.from_env()
-    # Verify that a running container does not exist already - docker does
-    # this for us (no to containers with the same name are allowed)
     try:
         client.containers.run(image = image_name,
                               detach = True,
                               environment = { "ACCEPT_EULA" : "Y",
                                               "MSSQL_PID" : "Developer",
                                               "MSSQL_SA_PASSWORD" : password },
-                              ports = {'1401/tcp' : ("127.0.0.1", 1433)},
+                              ports = {'1433/tcp' : ("127.0.0.1", 1433)},
                               cap_add = ["SYS_PTRACE"],
                               volumes = volumes,
                               restart_policy = {"Name" : "on-failure",
                                                 "MaximumRetryCount" : 3},
-                              name = container_name)
+                             name = container_name)
         click.echo("Started")
-        if mode == "attach":
-            # Run-Query Attach Database
-            click.echo("Attaching the anpr database...")
-        else:
-            # Run-Query Restore Database
-            click.echo("Restoring the anpr database...")
-
     except docker.errors.APIError as e:
         click.echo(e)
     except docker.errors.ContainerError as e2:
         click.echo(e2)
+
+
+@click.option('--password', '-p',
+             type = str,
+             envvar = 'SQL_PASSWORD',
+             required = True,
+             help="Database password for SA user. Read from environment variable 'SQL_PASSWORD'")
+@click.option('--dbname', type = str, default = 'CortexDBWarehouse', help="Name of database to restore")
+@click.option('--name-map', type = str, help="Dictionary with the components of the MOVE clause that make up the restore database sql query. Format should be \"{componentName : componentFilename}\"")
+@click.option('--verbose', '-v', is_flag=True)
+@click.option('--bak-filename', type = str, default = 'CortexDBWarehouse_20170705.bak', help="Name of database backup file (just name, not path to)")
+@anpr.command('restore', short_help="Restore the database")
+def restore(password, dbname, name_map, verbose, bak_filename):
+    container = getContainer()
+    if not container:
+        click.echo("Server is not running")
+        return
+    # Try to convert input String to Dict (if available)
+    if name_map is None:
+        name_map = default_db_namemap
+    else:
+        name_map = ast.literal_eval(name_map)
+    # Check if bak-filename exists
+    if not os.path.isfile("".join([bakfile_path, "/", bak_filename])):
+        click.echo("No such bak file: {}".format("".join([bakfile_path, "/", bak_filename])))
+        sys.exit(1)
+    # Check if files already exist in target dir
+    for filename in name_map.values():
+        if os.path.isfile("".join([dbfiles_path, "/", filename])):
+            click.echo("Destination dbfile file already exists: {}".format("".join([dbfiles_path, "/", filename])))
+            sys.exit(1)
+
+    # Run Query through sqlcmd
+    query = query_restoredb_builder(dbname, name_map, bak_filename)
+    if verbose:
+        click.echo("\n[QUERY]")
+        click.echo(query)
+        click.echo("[---]")
+    response = container.exec_run(
+                       cmd = ["/opt/mssql-tools/bin/sqlcmd",
+                              "-U", "sa",
+                              "-P", password,
+                              "-S", "localhost",
+                              "-Q", query],
+                       detach = True)
+    if response != "":
+        click.echo(response)
+    click.echo("Restoring the database in the background")
+    click.echo("Run this script again with command restore-progress to query the progress of the operation.")
+
+@click.option('--password', '-p',
+             type = str,
+             envvar = 'SQL_PASSWORD',
+             required = True,
+             help = "Database password for SA user. Read from environment variable 'SQL_PASSWORD'")
+@click.option('--dbname', type = str, default = 'CortexDBWarehouse', help="Name of database to restore")
+@click.option('--name-map', type = str, help="Dictionary with the components of the MOVE clause that make up the restore database sql query. Format should be \"{componentName : componentFilename}\"")
+@click.option('--verbose', '-v', is_flag=True)
+@anpr.command('attach', short_help="Attach the anpr database")
+def attach(password, dbname, name_map, verbose):
+    """
+    Attach the anpr database (mdf, ldf files) to the running server.
+    """
+    container = getContainer()
+    if not container:
+        click.echo("Server is not running")
+        return
+    # Try to convert input String to Dict (if available)
+    if name_map is None:
+        name_map = default_db_namemap
+    else:
+        name_map = ast.literal_eval(name_map)
+    # Check if files dont exist in target dir
+    for filename in name_map.values():
+        if not os.path.isfile("".join([dbfiles_path, "/", filename])):
+            click.echo("Source dbfile does not exist: {}".format("".join([dbfiles_path, "/", filename])))
+            sys.exit(1)
+
+   # Run Query through sqlcmd
+    query = query_attachdb_builder(dbname, name_map)
+    if verbose:
+        click.echo("\n[QUERY]")
+        click.echo(query)
+        click.echo("[---]")
+    response = container.exec_run(
+                       cmd = ["/opt/mssql-tools/bin/sqlcmd",
+                              "-U", "sa",
+                              "-P", password,
+                              "-S", "localhost",
+                              "-Q", query],
+                       detach = False)
+    if response != "":
+        click.echo(response)
+    click.echo("Ok")
+
 
 @anpr.command('status', short_help="Status of the anpr-mssql-server")
 def get_status():
@@ -205,9 +336,12 @@ def get_status():
     container = getContainer()
     if container:
         click.echo(getContainer().status)
+    else:
+        click.echo("Stopped")
 
+@click.option('--force', '-f', is_flag=True)
 @anpr.command('stop', short_help="Stop the anpr-mssql-server")
-def stop_container():
+def stop_container(force):
     """
     Looks for a container named "anpr-mssql-server" and stop it if is running. Soft timeout of 15 seconds.
     """
@@ -216,11 +350,38 @@ def stop_container():
         click.echo("Server is not running")
         return
     try:
-        container.stop(timeout = 15)
+        if force:
+            timeout = 1
+        else:
+            timeout = 15
+        container.stop(timeout = timeout)
         container.remove()
         click.echo("Stopped and removed")
     except docker.errors.APIError as e:
         click.echo(e)
+
+
+@click.option('--password', '-p',
+             type = str,
+             envvar = 'SQL_PASSWORD',
+             required = True,
+             help="Database password for SA user. Read from environment variable 'SQL_PASSWORD'")
+@anpr.command('restore-progress', short_help="Show restore database progress")
+def show_db_restore_progress(password):
+    """
+    Show the progress of the restore database operation
+    """
+    container = getContainer()
+    if not container:
+        click.echo("Server is not running")
+        return
+    response =container.exec_run(
+                       cmd = ["/opt/mssql-tools/bin/sqlcmd",
+                              "-U", "sa",
+                              "-P", password,
+                              "-S", "localhost",
+                              "-Q", query_restore_progress_builder()])
+    click.echo(response)
 
 ##############################################
 ##############################################
